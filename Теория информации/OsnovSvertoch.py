949879a375b082a27267b028a2669c19e5f58d90
Anton <block2456@gmail.com>
2022-04-05 22:04:35 +0300

    Ввод кол-ва сумматоров и перевод в бинарную последовательность

--- Теория информации/OsnovSvertoch.py
+++ Теория информации/OsnovSvertoch.py
@@ -2,3 +2,23 @@ import numpy as np
 import binascii
 from tkinter import *
 import tkinter.messagebox as mb
+
+n=int(input("Введите количество сумматоров: "))
+posled=input("Введите последовательность: ")
+
+
+def text_to_bits(posled, encoding='utf-8', errors='surrogatepass'):
+    bits = bin(int(binascii.hexlify(posled.encode(encoding, errors)), 16))[2:]
+    return bits.zfill(8 * ((len(bits) + 7) // 8))
+
+
+spisok_text_to_bit = text_to_bits(posled)
+spis_posled=[]
+for i in range(len(spisok_text_to_bit)):
+    spis_posled.append(spisok_text_to_bit[i])
+
+
+print(spisok_text_to_bit)
+
+
+print("Введённая последовательность в бинарном представлении: ",spis_posled)
